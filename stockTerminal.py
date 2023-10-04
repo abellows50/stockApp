834c6a2c47c67a1e8e4ede36912fb1b4534224bf
Abel Bellows <abellows50@stuy.edu>
2023-10-04 19:31:40 -0400

    worked on my code for the terminal based program. Switching from windows to replit

--- stockTerminal.py
+++ stockTerminal.py
@@ -1,5 +1,27 @@
 import stock
+import os
+
+class User:
+    def __init__(self,user):
+        with open(f"{os.getcwd()}programFiles\\{user}.logf","r") as userFile:
+            userData = userFile.readLines()
+
 
 user = input("Username: ")
+# with open("programFiles/users.csv") as users:
+#     myUsers = user.readlines()
+#     myUsers = [u.split(",") for u in myUsers]
+#     file = False
+#     for auser in myUsers:
+#         if auser[0] == user:
+#             myPass = input("Password")
+#             if myPass == auser[1]:
+#                 file = auser[2]
+#                 break
+#     if not file:
+#         myUsers.append("")     
+     
+
+theuser = User(user)
 stock = stock.makeSearch(input("Stock: "))
 print(stock)
